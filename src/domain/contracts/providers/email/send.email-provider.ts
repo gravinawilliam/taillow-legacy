b8b7e9f8e488826eb42e817eecad0c1f9eb57f4b
William Gravina <dev.gravinawilliam@gmail.com>
2022-07-28 01:10:46 -0300

    ♻️ refactor: added fields in return success

--- src/domain/contracts/providers/email/send.email-provider.ts
+++ src/domain/contracts/providers/email/send.email-provider.ts
@@ -1,5 +1,6 @@
 import { ProviderError } from '@domain/entities/errors/shared/provider.error';
 import { EmailContact } from '@domain/entities/models/email/email-contact.model';
+import { ProvidersSendEmail } from '@domain/entities/models/email/email-registered-in-providers.model';
 
 import { Either } from '@shared/utils/either.util';
 
@@ -12,7 +13,10 @@ export namespace SendEmailProviderDTO {
   };
 
   export type ResultError = ProviderError;
-  type ResultSuccess = undefined;
+  type ResultSuccess = {
+    result: unknown;
+    provider: ProvidersSendEmail;
+  };
 
   export type Result = Promise<Either<ResultError, ResultSuccess>>;
 }
